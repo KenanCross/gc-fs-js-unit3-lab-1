0d2afc1951dde3bf6863b677af01327ad577d53c
kenancross <kenancross@gmail.com>
2024-08-20 17:30:13 -0400

    Finished assignment

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const firstName = 'Ken';
-const lastName = 'Cross';
-const fullName = `${firstName} ${lastName}`;
-console.log(fullName);
+refresher();
+
+function refresher() {
+	const firstName = "Ken";
+	const lastName = "Cross";
+	const fullName = `${firstName} ${lastName}`;
+	console.log(fullName);
+	let population = 620376;
+	console.log(population);
+	console.log(population * 3);
+	let javaScriptIsCool = true;
+	let colors = ["red", "purple", "orange", "yellow", "green"];
+	let i = 1;
+
+	while (i <= 5) {
+		console.log(i);
+		i++;
+	}
+	i = 0;
+	while (i <= 4) {
+		console.log(i);
+		i++;
+	}
+}
